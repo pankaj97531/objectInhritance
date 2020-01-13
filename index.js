1621a5a5c1a78c3d742106617245a92291258a3d
adubey <adubey@focusvision.com>
2020-01-13 14:52:48 +0530

    commit

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ clone.property2 = "property2222";
 console.dir(Object.getPrototypeOf(testObj1));
 
 console.log(clone.property2);
+
+//https://javascript.info/prototype-methods
+
+let animal = {
+  eats: true
+};
+
+function Rabbit(name) {
+  this.name = name;
+}
+
+Rabbit.prototype = animal;
+
+let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+
+console.log( rabbit.eats ); // true
+
+///https://javascript.info/function-prototype
+
+//https://javascript.info/constructor-new
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
